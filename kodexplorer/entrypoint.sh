00d53b9fa5c583547112e531fe3e2bba5e5d0715
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2019-07-03 18:06:49 +0800

    fix

--- kodexplorer/entrypoint.sh
+++ kodexplorer/entrypoint.sh
@@ -6,5 +6,8 @@ if [ "$1" = 'php' ] && [ "$(id -u)" = '0' ]; then
     chown -R www-data /var/www/html
     chmod -R 777 /var/www/html/
 fi
+if [ ! -e '/var/www/html/index.php' ]; then
+    cp -a /usr/src/kodexplorer/* /var/www/html/
+fi
 
 exec "$@"
