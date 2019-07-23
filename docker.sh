9e31945d60eb6dda0d3a9f4a43def64b2b19d8e3
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2019-07-23 09:44:51 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                                               
 #===================================================================#
 #   System Required:  CentOS 7                                 #
-#   Description: Install v2ray for CentOS7 #
+#   Description: Install Docker for Linux #
 #   Author: Azure <2894049053@qq.com>                               #
 #   github: @baiyutribe <https://github.com/baiyuetribe>                     #
 #   Blog:  佰阅部落 https://baiyue.one                           #
@@ -14,13 +14,38 @@
 #  |  |_)  |  /  _____  \  |  |     |  |     |  `--'  | |  |____  __|  `--'  | |  |\   | |  |____ 
 #  |______/  /__/     \__\ |__|     |__|      \______/  |_______|(__)\______/  |__| \__| |_______|
 #
-#一键脚本
+#一键脚本(智能检测docker环境，若存在则跳过，若不存在，则执行安装)
 # bash <(curl -L -s https://raw.githubusercontent.com/Baiyuetribe/v2ray_docker/master/v2ray.sh)             
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#check root
+[ $(id -u) != "0" ] && { echo "错误: 您必须以root用户运行此脚本"; exit 1; }
+
+#工具安装
+install_pack() {
+    pack_name="wget未知"
+    echo "===> Start to install curl"    
+    if [ -x "$(command -v yum)" ]; then
+        command -v curl > /dev/null || yum install -y curl
+    elif [ -x "$(command -v apt)" ]; then
+        command -v curl > /dev/null || apt install -y curl
+    else
+        echo "Package manager is not support this OS. Only support to use yum/apt."
+        exit -1
+    fi
+    
+}
+
 # @安装docker
 install_docker() {
     docker version > /dev/null || curl -fsSL get.docker.com | bash 
     service docker restart 
     systemctl enable docker  
+}
+install_docker_compose() {
+	curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
 }
 
 # 单独检测docker是否安装，否则执行安装docker。
@@ -31,7 +56,17 @@ check_docker() {
 	else
 		echo "开始安装docker。。。"
 		# command
-		install_docker
+		install_docker        
+	fi
+}
+check_docker_compose() {
+	if [ -x "$(command -v docker-compose)" ]; then
+		echo "docker-compose is installed"
+		# command
+	else
+		echo "Install docker-compose"
+		# command
+		install_docker_compose
 	fi
 }
 
@@ -63,7 +98,9 @@ start_menu(){
     read -p "是否继续？:" num
     case "$num" in   
     *)
+    install_pack
 	check_docker
+    check_docker_compose
     echo -e "\033[32m====================================\033[0m"	
     echo -e "\033[32m 恭喜，您已经完成docker的安装                        "
     echo -e "\033[32m====================================\033[0m"	
@@ -72,4 +109,3 @@ start_menu(){
 }
 
 start_menu
-
